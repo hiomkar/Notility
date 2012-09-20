347796352d5908992fa568671ae2d96e0ec796b0
unknown <hiomkar@gmail.com>
2012-09-20 17:34:17 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notility::Application.config.secret_token = '609eb8278df64d4a6d27e68f882d0d726b2959272295d5fb6d84ca47261022ce8e97fd0294e2f7d5064edbc3478037d17cb1621cda0e4089d7e87a387e5c995e'
