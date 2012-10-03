10a84d9d70d80f12b64689c869a94dcb06970e38
Omkar <hiomkar@gmail.com>
2012-10-03 03:25:47 -0400

    db fix

--- db/migrate/20120923054042_add_tag_index_to_note.rb
+++ db/migrate/20120923054042_add_tag_index_to_note.rb
@@ -1,5 +1,6 @@
-class AddIndexToNote < ActiveRecord::Migration
+class AddTagIndexToNote < ActiveRecord::Migration
   def change
+    add_column :notes, :tag, :string
     add_index(:notes, [:user_id, :created_at], :name => 'by_user')
   end
 end
