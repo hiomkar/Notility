085127439b219e613e11657610914638c4dfafff
Omkar <hiomkar@gmail.com>
2012-10-03 02:20:38 -0400

    modified:   .idea/dataSources.ids 	modified:   .idea/dataSources.xml 	modified:   .idea/workspace.xml 	new file:   db/structure.sql 	new file:   push_201210030703.dat 	new file:   push_201210030706.dat 	new file:   push_201210030708.dat 	deleted:    test/fixtures/.gitkeep 	deleted:    test/fixtures/courses.yml 	deleted:    test/fixtures/notes.yml 	deleted:    test/fixtures/users.yml 	deleted:    test/functional/.gitkeep 	deleted:    test/functional/courses_controller_test.rb 	deleted:    test/functional/notes_controller_test.rb 	deleted:    test/functional/users_controller_test.rb 	deleted:    test/integration/.gitkeep 	deleted:    test/performance/browsing_test.rb 	deleted:    test/test_helper.rb 	deleted:    test/unit/.gitkeep 	deleted:    test/unit/course_test.rb 	deleted:    test/unit/helpers/courses_helper_test.rb 	deleted:    test/unit/helpers/notes_helper_test.rb 	deleted:    test/unit/helpers/users_helper_test.rb 	deleted:    test/unit/note_test.rb 	deleted:    test/unit/user_test.rb

---
+++ db/structure.sql
@@ -0,0 +1,29 @@
+INSERT INTO schema_migrations (version) VALUES ('20120920224305');
+
+INSERT INTO schema_migrations (version) VALUES ('20120921014014');
+
+INSERT INTO schema_migrations (version) VALUES ('20120921014923');
+
+INSERT INTO schema_migrations (version) VALUES ('20120921015722');
+
+INSERT INTO schema_migrations (version) VALUES ('20120923042850');
+
+INSERT INTO schema_migrations (version) VALUES ('20120923045128');
+
+INSERT INTO schema_migrations (version) VALUES ('20120923051704');
+
+INSERT INTO schema_migrations (version) VALUES ('20120923052001');
+
+INSERT INTO schema_migrations (version) VALUES ('20120923053742');
+
+INSERT INTO schema_migrations (version) VALUES ('20120923054042');
+
+INSERT INTO schema_migrations (version) VALUES ('20120923055700');
+
+INSERT INTO schema_migrations (version) VALUES ('20120923060054');
+
+INSERT INTO schema_migrations (version) VALUES ('20120923060517');
+
+INSERT INTO schema_migrations (version) VALUES ('20120923060623');
+
+INSERT INTO schema_migrations (version) VALUES ('20120923064847');
